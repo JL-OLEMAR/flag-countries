d39a6d8e119c8d81f073b5290b0bff6fc6389b48
JL-OLEMAR <joseluis19963@gmail.com>
2021-06-28 18:25:59 -0500

    flag countries

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import CountryPage from './country-page'
+import CountryList from './country-list'
+import ActionList from './action-list'
+import reducer from './reducer'
+import Header from './header'
+import './App.css'
+
+const initialState = {
+  countryList: [],
+  countryListByName: [],
+  coutryFilteredByRegion: [],
+  filterByRegion: ''
 }
 
-export default App;
+const store = createStore(reducer, initialState)
+
+function App () {
+  const [darkMode, setDarkMode] = useState(false)
+  // eslint-disable-next-line no-unused-vars
+  const [checked, setChecked] = useState(false)
+  const mainClass = darkMode ? 'is-dark-mode' : 'is-light-mode'
+
+  function changeMedia (mq) {
+    setDarkMode(mq.matches)
+    setChecked(mq.matches)
+  }
+
+  useEffect(() => {
+    const mq = window.matchMedia('(prefers-color-scheme: dark)')
+    mq.addListener(changeMedia)
+    setDarkMode(mq.matches)
+    setChecked(mq.matches)
+    return () => {
+      mq.removeListener(changeMedia)
+    }
+  }, [])
+  return (
+    <main className={mainClass}>
+      <Provider store={store}>
+        <Router>
+          <Header setDarkMode={setDarkMode} darkMode={darkMode} />
+          <Switch>
+            <Route path='/country/:id' component={CountryPage} />
+            <Route path='/'>
+              <ActionList />
+              <CountryList />
+            </Route>
+          </Switch>
+        </Router>
+      </Provider>
+    </main>
+  )
+}
+
+export default App
